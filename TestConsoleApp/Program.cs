02a1b193a11e60e0c45cec0603ec390ca604c7a7
mcilis <mcilis@hotmail.com>
2014-05-11 22:49:34 +0300

    initial

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace InteractiveWebDriver
+namespace TestConsoleApp
 {
     class Program
     {
--- TestConsoleApp/Properties/AssemblyInfo.cs
+++ TestConsoleApp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("InteractiveWebDriver")]
+[assembly: AssemblyTitle("TestConsoleApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("InteractiveWebDriver")]
+[assembly: AssemblyProduct("TestConsoleApp")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
